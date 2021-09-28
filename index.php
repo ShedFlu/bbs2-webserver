0e4c235e8f3374ff7d766b2013cab48f26672ae4
Jonas <44437149+ShedFlu@users.noreply.github.com>
2021-09-28 11:41:07 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+      
+  </head>
+  <body>
+    <h1>Heftige Stundenpläne, Bruder!</h1>
+    <a href="/Stundenplan.html">Stundenplan</a>
+    
+  </body>
+</html>
